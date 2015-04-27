58fd5be2da789bfe22484692373e46b4a5f3d109
Lucas Jenss <lucas@x3ro.de>
2015-04-27 13:29:22 +0200

    Fix recursion endpoint of euler and rk2

--- src/Iterationsgleichungen.hs
+++ src/Iterationsgleichungen.hs
@@ -1,16 +1,19 @@
-module Iter where
+module Iterationsgleichungen where
 -- gehe davon aus dass xEnd das maximum für x ist...
 
-main = print (euler 0.001 0.2)
+-- main = print (euler 0.001 0.2)
 
 f' x y = 10 - 500 * y + 500 * x
 
 --------------------------------------------------------------------------------
+--- foobar h x_end = zipWith [0..] (euler h x_end)
+
+
 euler h x_end = y_next_e 1 h 0 x_end []
 
 y_next_e:: Double -> Double -> Double -> Double -> [Double] -> [Double]
 y_next_e y h t_n x_end ys
-    | t_n == x_end = ys -- rekursionsanker
+    | t_n >= x_end = ys -- rekursionsanker
     | otherwise    = y_next_e f'_curr h t_next x_end (f'_curr:ys)
         where t_next  = t_n+h
               f'_curr = y+h * (f' t_n y)
@@ -20,7 +23,7 @@ rk2 h x_end = y_next_rk 1 h 0 x_end []
 
 y_next_rk:: Double -> Double -> Double -> Double -> [Double] -> [Double]
 y_next_rk y h t_n x_end ys
-    | t_n == x_end = ys -- rekursionsanker
+    | t_n >= x_end = ys -- rekursionsanker
     | otherwise    = y_next_rk f'_curr h t_next x_end (f'_curr:ys)
         where f'_curr = (y+k2)
               t_next  = t_n+h
