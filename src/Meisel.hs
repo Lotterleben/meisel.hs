f380183ef9bb385f0d55ad8a95f26ff003c9bc56
Lotte Steenbrink <lotte@zombietetris.de>
2015-04-27 13:31:36 +0200

    init

--- src/Meisel.hs
+++ src/Meisel.hs
@@ -1,11 +1,14 @@
 module Meisel where
 
 erdradius:: Double
-erdradius = 6378            -- km
+erdradius = 6378             -- km
 erdmasse :: Double
 erdmasse = 5.9736*10**24     -- kg
 gravitation :: Double
 gravitation = 66.743*10**(-12) -- m^3/(kg*s^2)
+gravitation_constant :: Double
+gravitation_constant = 9.81  -- m/(s^2)
+
 
 data Vector2D = Vector2D Double Double deriving (Show)
 
@@ -14,3 +17,5 @@ startposition winkel hoehe = Vector2D (cos(winkel)*gesamthoehe) (sin(winkel)*ges
     where gesamthoehe = erdradius + hoehe
 
 
+fall :: (Num a) => a -> a -> [a]
+fall height obj_weight = TODO
